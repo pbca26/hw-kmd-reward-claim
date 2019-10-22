71bc3048911d7db1b61554384f45083f3d1754ad
pbca26 <pbca26@gmail.com>
2019-10-22 19:29:49 +0300

    fix sass compile issue

--- config-overrides.js
+++ config-overrides.js
@@ -1,1 +1,8 @@
-module.exports = require('react-app-rewire-hot-loader');
+const hotLoader = require('react-app-rewire-hot-loader');
+const rewireSass = require('react-app-rewire-scss');
+
+module.exports = function override(config, env) {
+  config = hotLoader(config, env);
+  config = rewireSass(config, env);
+  return config;
+}
