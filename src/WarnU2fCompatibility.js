a5bdbf410baeafdd9dff2904c29a079e9ed59646
pbca26 <pbca26@gmail.com>
2020-06-16 15:54:12 +0300

    browser compatibility extend to chrome, chromium and brave, exclude opera

--- src/WarnU2fCompatibility.js
+++ src/WarnU2fCompatibility.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import {isSupported} from 'u2f-api';
+import {isChrome, isOpera} from 'react-device-detect';
 import Modal from './Modal';
 
 class WarnU2fCompatibility extends React.Component {
   state = {
-    u2fSupported: null
+    u2fSupported: null,
+    isChrome: null,
+    isOpera,
   };
 
   async componentDidMount() {
     this.setState({
-      u2fSupported: await isSupported()
+      u2fSupported: await isSupported(),
+      isChrome,
+      isOpera,
     });
   }
 
   render() {
     return (
       <Modal
-        title="Warning: U2F Not Supported in This Browser"
-        show={this.state.u2fSupported === false}>
-        <p>The U2F API is required for the web browser to communicate with the <span className="ucfirst">{this.state.vendor}</span> device.</p>
-        <p>Try using Chrome or Opera instead.</p>
-        <p>You can view a list of U2F supporting browsers at <a target="_blank" rel="noopener noreferrer" href="https://caniuse.com/#feat=u2f">caniuse.com/#feat=u2f</a>.</p>
+        title="Warning: U2F/WEBUSB Not Supported in This Browser"
+        show={
+          (this.state.u2fSupported === false && this.state.isChrome === false) ||
+          this.state.isOpera === true
+        }>
+        <p>The U2F/WEBUSB API is required for the web browser to communicate with the <span className="ucfirst">{this.state.vendor}</span> device.</p>
+        <p>Try using Chrome, Chromium or Brave instead.</p>
+        <p>You can view a list of U2F/WEBUSB supporting browsers at <a target="_blank" rel="noopener noreferrer" href="https://caniuse.com/#feat=u2f">U2F</a>, <a target="_blank" rel="noopener noreferrer" href="https://caniuse.com/#feat=webusb">WEBUSB</a>.</p>
       </Modal>
     );
   }
