991c877515d017899915d77c2013e876429cf7e0
pbca26 <pbca26@gmail.com>
2020-08-12 18:13:46 +0300

    lib get trezor firmware version method

---
+++ src/lib/trezor-hw-version.js
@@ -0,0 +1,43 @@
+import TrezorConnect from 'trezor-connect';
+
+const RECHECK_TIMEOUT = 1000;
+
+const trezorCheckFW = async () => {
+  return new Promise(async(resolve, reject) => {
+    const interval = setInterval(async() => {
+      const result = await TrezorConnect.getFeatures();
+      console.warn(result);
+
+      if (result &&
+          result.success === true) {
+        if (result.payload.hasOwnProperty('major_version') &&
+            result.payload.hasOwnProperty('minor_version') &&
+            result.payload.hasOwnProperty('patch_version') &&
+            result.payload.hasOwnProperty('model')) {
+          const {
+            major_version,
+            minor_version,
+            patch_version,
+            model,
+          } = result.payload;
+
+          clearInterval(interval);
+          resolve({
+            major_version,
+            minor_version,
+            patch_version,
+            model,
+          });
+        } else {
+          console.warn('unable to check trezor firmware version');
+          //resolve(false);
+        }
+      } else {
+        console.warn('unable to check trezor firmware version');
+        //resolve(false);
+      }
+    }, RECHECK_TIMEOUT);
+  });
+};
+
+export default trezorCheckFW;
